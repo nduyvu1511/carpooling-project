8751b08ce86713e9d372fdfe27036934dde6913e
vu <duyvujarvan4@gmail.com>
2022-06-26 16:11:36 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ module.exports = withImages({
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
-    domains: [
-      process.env.NEXT_PUBLIC_IMAGE_URL,
-      "cf.shopee.vn",
-      process.env.NEXT_PUBLIC_IMAGE_URL2,
-      "f12.photo.talk.zdn.vn",
-      "192.168.1.38",
-    ],
+    domains: ["quanly.exxe.vn"],
   },
 })
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-// export const API_URL = "https://quanly.exxe.vn"
-const API_LINK = "192.168.1.38"
-export const API_URL = `http://${API_LINK}:1234`
+export const API_URL = "https://quanly.exxe.vn"
+// const API_LINK = "192.168.1.38"
+// export const API_URL = `http://${API_LINK}:1234`
 // export const IMAGE_URL = API_URL
 
 export const DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_URL
